dcac276aef4ed2b01c06f029494b582337559ed1
Boomish-Tendency <bklindeboom@gmail.com>
2020-10-16 12:29:00 -0400

    poem

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -10,21 +10,30 @@ let lovesCode = true;
 //Create a variable called 'age' that has a value equal to your age as a number. 
 
 // Code Here
-
+let age = 33
 //////////////////STEP 3////////////////////
 //Create a variable called 'name' and assign it a value of your name as a string.  
 
 // Code Here
-
+let name = 'Brian'
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
 // Code Here
+let phrase = ""
+  //////////////////STEP 5////////////////////
+  //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
-//////////////////STEP 5////////////////////
-//Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
+  // Code Here
+    lovesCode
+  ? (phrase = `I love code`)
+  : (phrase = `I do not love to code`);
 
-// Code Here
+// if (lovesCode === true) {
+//   phrase = "I love code";
+// } else {
+//   phrase = "I do not to code";
+// }
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
